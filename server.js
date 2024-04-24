acde79d2e04f65706a373f8974715af79b144ee8
Troy Latchman <troylatchman@gmail.com>
2024-04-24 11:53:41 -0400

    simple routing

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  console.log(`Request URL: ${req.url}`);
-  console.log(`Request method: ${req.method}`);
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World!</h1>");
-  //   res.statusCode = 404;
-  //   res.writeHead(500, { "Content-Type": "application/json" });
-  //   res.end(JSON.stringify({ message: "Server Error" }));
+  try {
+    // check if GET request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Home Page</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("<h1>Server Error</h1>");
+  }
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
