23f9e073500c35d4330212e80f37b8a61bba293d
Troy Latchman <troylatchman@gmail.com>
2024-04-24 10:32:59 -0400

    export default

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { getPosts } from "./postController.js";
+import getPosts, { getPostsLength } from "./postController.js";
 
 // const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
 // console.log(`Random Number: ${generateRandomNumber()}`);
 // console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`);
 
 console.log(getPosts());
+console.log(`Post length: ${getPostsLength()}`);
