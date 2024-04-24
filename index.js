8909aec5f424b4542f08a06e4523c3ef88270103
Troy Latchman <troylatchman@gmail.com>
2024-04-24 10:24:57 -0400

    migrate from commonjs to esmodule inline export

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
-console.log(`Random Number: ${generateRandomNumber()}`);
-console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`);
+import { getPosts } from "./postController.js";
+
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`);
+
+console.log(getPosts());
