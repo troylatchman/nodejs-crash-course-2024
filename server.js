66b3a2bf520e26a597d11495e17ae8e13fa179ce
Troy Latchman <troylatchman@gmail.com>
2024-04-24 11:26:40 -0400

    install nodemon package and create script

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import http from "http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  //   res.setHeader("Content-Type", "text/html");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello World!</h1>");
   //   res.statusCode = 404;
-  res.writeHead(500, { "Content-Type": "application/json" });
-  //   res.write("<h1>Hello World!</h1>");
-  res.end(JSON.stringify({ message: "Server Error" }));
+  //   res.writeHead(500, { "Content-Type": "application/json" });
+  //   res.end(JSON.stringify({ message: "Server Error" }));
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
