0ee7505e9b5ef4feda47134e580cfa359e579ed7
Troy Latchman <troylatchman@gmail.com>
2024-04-26 21:36:57 -0400

    post request body

--- server2.js
+++ server2.js
@@ -38,6 +38,22 @@ const getUserByIdHandler = (req, res) => {
   res.end();
 };
 
+// Route handler for POST /api/users
+const createUserHandler = (req, res) => {
+  let body = "";
+  // listen for the data
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    const newUser = JSON.parse(body);
+    users.push(newUser);
+    res.statusCode = 201;
+    res.write(JSON.stringify(newUser));
+    res.end();
+  });
+};
+
 // Not found handler
 const notFoundHandler = (req, res) => {
   res.statusCode = 404;
@@ -55,6 +71,8 @@ const server = createServer((req, res) => {
         req.method === "GET"
       ) {
         getUserByIdHandler(req, res);
+      } else if (req.url === "/api/users" && req.method === "POST") {
+        createUserHandler(req, res);
       } else {
         notFoundHandler(req, res);
       }
